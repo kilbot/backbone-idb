1e559eb9f97cb2f7be633f9518cc20dd81f945f7
Paul Kilmurray <paul@kilbot.com.au>
2014-11-11 12:02:46 +0800

    missed one :)

--- backbone-idb.js
+++ backbone-idb.js
@@ -19,7 +19,7 @@
       return factory(_ || global._, Backbone || global.Backbone, IDBStore || global.IDBStore, $ || global.$, 'AMD');
     });
   } else {
-    factory(_, Backbone, IDBStore, global);
+    factory(_, Backbone, IDBStore, $, global);
   }
 }(this, function(_, Backbone, IDBStore, $, global) {
   'use strict';
